86107fe8d3a18b059e7228f726c60456ea9d3a85
Thungpuen <thungpuen@gmail.com>
2016-06-17 13:48:28 +0700

    update new user finish

--- app/src/main/java/nsru/noknoi/puwanat/cartoonbook/SignUpActivity.java
+++ app/src/main/java/nsru/noknoi/puwanat/cartoonbook/SignUpActivity.java
@@ -5,6 +5,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
 public class SignUpActivity extends AppCompatActivity {
 
     //Explicit
@@ -50,11 +60,43 @@ public class SignUpActivity extends AppCompatActivity {
             MyAlert myAlert = new MyAlert();
             myAlert.myDialog(this, "มีช่องว่าง","" +
                     "กรุณากรอกทุกช่อง");
+
         } else {
                 //No Space
+            uploadNewUserToServer();
         }
 
     }   //click sign
+
+    private void uploadNewUserToServer() {
+
+        String urlPost = "http://swiftcodingthai.com/gun/add_user_gun.php";
+        OkHttpClient okHttpClient = new OkHttpClient();
+        RequestBody requestBody = new FormEncodingBuilder()
+                .add("isAdd", "true")
+                .add("Name", nameString)
+                .add("Surname", surnameString)
+                .add("Address", addressString)
+                .add("Phone", phoneString)
+                .add("Username", usernameString)
+                .add("Password", passwordString)
+                .build();
+        Request.Builder builder = new Request.Builder();
+        Request request = builder.url(urlPost).post(requestBody).build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                    finish();
+            }
+        });
+
+    }   //UPLOAD
 
 
 }   //Main Class
