29b9cbe7e304991acdff84057a2da8ebce879c9e
Thungpuen <thungpuen@gmail.com>
2016-06-17 10:30:27 +0700

    Intent to SignUp finish

--- app/src/main/java/nsru/noknoi/puwanat/cartoonbook/MainActivity.java
+++ app/src/main/java/nsru/noknoi/puwanat/cartoonbook/MainActivity.java
@@ -1,7 +1,9 @@
 package nsru.noknoi.puwanat.cartoonbook;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }   //main method
+
+    public void clickSignUpMain(View view) {
+        startActivity(new Intent(MainActivity.this,SignUpActivity.class));
     }
-}
+
+}   //main class
