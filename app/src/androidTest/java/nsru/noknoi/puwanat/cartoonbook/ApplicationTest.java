aa4d9992490a8a3322a69984fa2959a16da8ab0a
Thungpuen <thungpuen@gmail.com>
2016-06-17 09:46:59 +0700

    Start App

---
+++ app/src/androidTest/java/nsru/noknoi/puwanat/cartoonbook/ApplicationTest.java
@@ -0,0 +1,13 @@
+package nsru.noknoi.puwanat.cartoonbook;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
