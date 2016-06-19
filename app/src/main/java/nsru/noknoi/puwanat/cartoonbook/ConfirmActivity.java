21b0a86d3f7d955ef437927780198207ab57c4bc
Thungpuen <thungpuen@gmail.com>
2016-06-19 10:00:16 +0700

    design confirm finish

--- app/src/main/java/nsru/noknoi/puwanat/cartoonbook/ConfirmActivity.java
+++ app/src/main/java/nsru/noknoi/puwanat/cartoonbook/ConfirmActivity.java
@@ -2,6 +2,7 @@ package nsru.noknoi.puwanat.cartoonbook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ConfirmActivity extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class ConfirmActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm);
+    }   //method
+
+    public void clickAddMore(View view) {
+        finish();
     }
-}
+
+    public void clickOrder(View view) {
+
+    }   //clickOrder
+
+}   //main class
